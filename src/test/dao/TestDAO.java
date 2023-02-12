ead6aff4bd1840ad548b2c03fd043d5206518cb0
opentutorials <{jse3685545@naver.com}>
2023-02-12 18:47:17 +0900

    02-12

--- src/test/dao/TestDAO.java
+++ src/test/dao/TestDAO.java
@@ -3,9 +3,22 @@ package test.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
 import test.vo.UserVO;
 
-public interface TestDAO {
+@Repository("testDAO")
+public class TestDAO {
 	
-	List<UserVO> selectTest() throws SQLException;
+	@Resource(name="sqlSession")
+	private SqlSession session;
+	
+	
+	public List<UserVO> selectTest() throws SQLException {
+		return session.selectList("test.mapper.selectTest");
+	}
+
 }
