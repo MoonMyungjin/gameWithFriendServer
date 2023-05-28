dd32127d5cfc5d6f1a9cb27b3a67218d7b5c730e
yoZing <josephkim525@gmail.com>
2023-05-28 20:21:49 +0900

    20230529 returnMap camelCase 변환을 위한 util Class 정의 작업자 : 김요셉

---
+++ src/util/CustomMap.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.LinkedHashMap;
+
+import com.google.common.base.CaseFormat;
+
+public class CustomMap extends LinkedHashMap<String, Object> {
+	
+	@Override
+    public Object put(String key, Object value) {
+        return super.put(toLowerCamel((String) key), value);
+    }
+
+    private static String toLowerCamel(String key) {
+        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, key);
+    }
+}
