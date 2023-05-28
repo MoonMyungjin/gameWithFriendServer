6edf35b3d0c4ada51c4d83bd174bb641d7fb115d
yoZing <josephkim525@gmail.com>
2023-05-29 01:17:31 +0900

    commit

--- src/base/login/service/LoginServiceImpl.java
+++ src/base/login/service/LoginServiceImpl.java
@@ -1,10 +1,10 @@
 package base.login.service;
 
 import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import base.login.dao.LoginDAO;
@@ -22,14 +22,18 @@ public class LoginServiceImpl implements LoginService {
 		returnMap = loginDAO.selectUserInfo(commandMap);
 		
 		HashMap<String, Object> paramMap = new HashMap<String, Object>();
-		if (returnMap == null) {
+		System.out.println("------------------------------------------" + returnMap.size());
+		System.out.println("------------------------------------------" + (returnMap.size() == 1));
+		if (StringUtils.isEmpty(returnMap.get("uIntgId"))) {
 			// 신규 아이디를 insert 해주는로직을 넣자
+			System.out.println("------------------------------------------isnert");
 			paramMap.putAll(commandMap);
 			loginDAO.insertUserInfo(paramMap);
 			
 			returnMap = loginDAO.selectUserInfo(paramMap);
 		} else {
 			// 로그인으로 확인되면 정보 수정하고 return
+			System.out.println("------------------------------------------update");
 			loginDAO.updateUserLoginInfo(commandMap);
 			
 			returnMap = loginDAO.selectUserInfo(commandMap);
