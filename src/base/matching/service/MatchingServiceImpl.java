e4385d40d4b60fd1c8b7b05ab5798ab22e3abf1e
jhwiKim <134839934+jhwiKim@users.noreply.github.com>
2023-06-25 20:49:08 +0900

    push test

--- src/base/matching/service/MatchingServiceImpl.java
+++ src/base/matching/service/MatchingServiceImpl.java
@@ -11,7 +11,7 @@ import base.friend.dao.FriendDAO;
 import base.friend.service.FriendService;
 import base.matching.dao.MatchingDAO;
 import base.matching.vo.MatchingHistoryVO;
-
+ 
 @Service("MatchingService")
 public class MatchingServiceImpl implements MatchingService {
 	
