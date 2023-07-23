9a2eb53d657a188d78d377f409ddfada085d925d
opentutorials <{jse3685545@naver.com}>
2023-07-23 19:29:25 +0900

    jth 230723 소환사의 협곡 백단 커밋

--- src/gameMatching/controller/GameMatchingController.java
+++ src/gameMatching/controller/GameMatchingController.java
@@ -173,7 +173,8 @@ public class GameMatchingController {
 		codeVO.setCdDtlParentId("101");
 		List<CodeVO> selectOptionList = codeService.selectOptionList(codeVO);
 		if(gameType.equals("소환사의협곡")){
-			codeVO.setCdDtlParentId("109");
+			codeVO.setCdDtlParentId("10901");
+			selectOptionList = codeService.selectOptionList(codeVO);
 			dataMap.put("selectOptionList", selectOptionList);
 		}else if(gameType.equals("칼바람나락")){
 			selectOptionList.remove(1);
