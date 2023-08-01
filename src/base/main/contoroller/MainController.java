f15b06ec283737f755a568976ade50867b93010c
opentutorials <{jse3685545@naver.com}>
2023-08-02 00:01:05 +0900

    jth 230802

--- src/base/main/contoroller/MainController.java
+++ src/base/main/contoroller/MainController.java
@@ -11,11 +11,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import base.admin.vo.UserVO;
 import base.alram.service.AlramService;
 import base.alram.vo.AlramVO;
 import base.main.service.MainService;
@@ -83,6 +85,15 @@ public class MainController {
 		ResponseEntity<Map<String,Object>> entity  = new ResponseEntity<Map<String,Object>>(dataMap,HttpStatus.OK);
 		
 		return entity;
+	}
+	
+	
+	@RequestMapping(value = "/privatePolicy", method = RequestMethod.GET)
+	public String privatePolicy() throws Exception{
+		
+
+		
+		return "admin/privatePolicy";
 	}
 	
 	
