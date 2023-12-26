52ab8e877e70b52216d6886dae511571c8c6738a
yoZing <josephkim525@gmail.com>
2023-12-26 23:10:49 +0900

    작업자 : 김요셉 작업내용 : chatController 표준 형식으로반영

--- src/base/chat/controller/ChatController.java
+++ src/base/chat/controller/ChatController.java
@@ -7,14 +7,18 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.sonatype.aether.util.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.socket.WebSocketSession;
 
 import base.chat.service.TextChatService;
-import util.CustomMap;
+import util.HduoResponse;
+import util.Error;
 
 @RestController
 @RequestMapping(value="/chat")
@@ -24,50 +28,80 @@ public class ChatController {
 	@Resource(name="textChatService")
 	private TextChatService textChatService;
 	
-	@RequestMapping(value="/getChatList.do")
-	public ModelAndView getChatList(HttpServletRequest request, @RequestBody HashMap<String, Object> commandMap) throws Exception {
-		ModelAndView modelAndView = new ModelAndView("jsonView");
+	@RequestMapping(value="/getChatList.do", method=RequestMethod.GET)
+	public HduoResponse<List<Map<String,Object>>> getChatList(HttpServletRequest request, @RequestParam HashMap<String, Object> commandMap) throws Exception {
+		// 결과를 return하는 Hduo 공통 Response
+		HduoResponse<List<Map<String, Object>>> buildWith = null;
 		
 		List<Map<String, Object>> resultList = textChatService.getChatList(commandMap);
 		
-		modelAndView.addObject("chatList", resultList);
+		try {
+			buildWith = HduoResponse.create().succeed().buildWith(resultList);
+		} catch (Exception e) {
+			String msg = "에러가 발생했습니다.";
+			Error error = new Error(HttpStatus.INTERNAL_SERVER_ERROR, msg);
+			buildWith = HduoResponse.create().fail(error).buildWith(resultList);
+		}
 		
-		return modelAndView;
+		return buildWith;
 	}
 	
-	@RequestMapping(value="/openChatRoom.do")
-	public ModelAndView oepnChatRoom(HttpServletRequest requeest, @RequestBody HashMap<String, Object> commandMap) throws Exception {
-		ModelAndView modelAndView = new ModelAndView("jsonView");
+	@RequestMapping(value="/openChatRoom.do", method=RequestMethod.POST)
+	public HduoResponse<String> oepnChatRoom(HttpServletRequest requeest, @RequestBody HashMap<String, Object> commandMap) throws Exception {
+		// 결과를 return하는 Hduo 공통 Response
+		HduoResponse<String> buildWith = null;
 		
 		String chatRoomId = textChatService.openChatRoom(commandMap);
 		
-		Map<String, String> resultMap = new HashMap<String, String>();
-		resultMap.put("chatRoomId", chatRoomId);
+		// 리턴된 채팅방 값이 없을 경우 에러 발생
+		if (StringUtils.isEmpty(chatRoomId)) {
+			throw new Exception();
+		}
 		
-		modelAndView.addObject("resultMap", resultMap);
+		try {
+			buildWith = HduoResponse.create().succeed().buildWith(chatRoomId);
+		} catch (Exception e) {
+			String msg = "에러가 발생했습니다.";
+			Error error = new Error(HttpStatus.INTERNAL_SERVER_ERROR, msg);
+			buildWith = HduoResponse.create().fail(error).buildWith(chatRoomId);
+		}
 		
-		return modelAndView;
+		return buildWith;
 	}
 	
-	@RequestMapping(value="/selectChatter.do")
-	public ModelAndView selectChater(HttpServletRequest requeest, @RequestBody HashMap<String, Object> commandMap) throws Exception {
-		ModelAndView modelAndView = new ModelAndView("jsonView");
+	@RequestMapping(value="/selectChatter.do", method=RequestMethod.GET)
+	public HduoResponse<List<Map<String,Object>>> selectChater(HttpServletRequest requeest, @RequestBody HashMap<String, Object> commandMap) throws Exception {
+		// 결과를 return하는 Hduo 공통 Response
+		HduoResponse<List<Map<String, Object>>> buildWith = null;
 		
 		List<Map<String, Object>> resultList = textChatService.selectChatter(commandMap);
 		
-		modelAndView.addObject("resultList", resultList);
+		try {
+			buildWith = HduoResponse.create().succeed().buildWith(resultList);
+		} catch (Exception e) {
+			String msg = "에러가 발생했습니다.";
+			Error error = new Error(HttpStatus.INTERNAL_SERVER_ERROR, msg);
+			buildWith = HduoResponse.create().fail(error).buildWith(resultList);
+		}
 		
-		return modelAndView;
+		return buildWith;
 	}
 	
-	@RequestMapping(value="/selectUnreadMsg.do")
-	public ModelAndView selectUnreadMsg(HttpServletRequest requeest, @RequestBody HashMap<String, Object> commandMap) throws Exception {
-		ModelAndView modelAndView = new ModelAndView("jsonView");
+	@RequestMapping(value="/selectUnreadMsg.do", method=RequestMethod.GET)
+	public HduoResponse<List<Map<String,Object>>> selectUnreadMsg(HttpServletRequest requeest, @RequestBody HashMap<String, Object> commandMap) throws Exception {
+		// 결과를 return하는 Hduo 공통 Response
+		HduoResponse<List<Map<String, Object>>> buildWith = null;
 		
 		List<Map<String, Object>> resultList = textChatService.selectUnreadMsg(commandMap);
 		
-		modelAndView.addObject("resultList", resultList);
+		try {
+			buildWith = HduoResponse.create().succeed().buildWith(resultList);
+		} catch (Exception e) {
+			String msg = "에러가 발생했습니다.";
+			Error error = new Error(HttpStatus.INTERNAL_SERVER_ERROR, msg);
+			buildWith = HduoResponse.create().fail(error).buildWith(resultList);
+		}
 		
-		return modelAndView;
+		return buildWith;
 	}
 }
